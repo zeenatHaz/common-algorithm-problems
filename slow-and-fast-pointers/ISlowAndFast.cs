109f787346352d88983753fe1e32ad4ced0a1f37
Zeenat <zeena@LAPTOP-D1AN8IIS>
2022-03-17 11:49:16 -0700

    Added new function

--- slow-and-fast-pointers/ISlowAndFast.cs
+++ slow-and-fast-pointers/ISlowAndFast.cs
@@ -9,7 +9,9 @@ namespace slow_and_fast_pointers
     public interface ISlowAndFast
     {
         public bool HasCycle(ListNode head);
+        public ListNode DetectCycle(ListNode head);
         public bool IsHappy(int n);
         public ListNode MiddleNode(ListNode head);
+        public bool IsPalindrome(ListNode head);
     }
 }
--- slow-and-fast-pointers/SlowAndFast.cs
+++ slow-and-fast-pointers/SlowAndFast.cs
@@ -87,5 +87,37 @@ namespace slow_and_fast_pointers
 
             return slow;
         }
+
+        public bool IsPalindrome(ListNode head)
+        {
+            //1 -> 2 -> 2 -> 1 -> NULL
+
+            //1
+            //2
+            //2
+            //1
+            if (head == null)
+                return true;
+
+            Stack<int> stack = new Stack<int>();
+            ListNode curr = head;
+            while (curr != null)
+            {
+                stack.Push(curr.val);
+                curr = curr.next;
+            }
+
+            curr = head;
+            while (curr != null)
+            {
+                if (stack.Count == 0 || stack.Peek() != curr.val)
+                    return false;
+
+                stack.Pop();
+                curr = curr.next;
+            }
+
+            return stack.Count == 0;
+        }
     }
 }
