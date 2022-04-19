9b938fcd2aaa182806f89ee14c2549abc3d20f52
Zeenat <zeena@LAPTOP-D1AN8IIS>
2022-04-19 12:31:27 -0700

    new problems.

---
+++ misc/Program.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace misc
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] time = { 30, 20, 150, 100, 40 };
+            int[] arr = { 1, 1, 2, 45, 46, 46 };int target = 47;
+            // NumPairsDivisibleBy60(time);
+            // twoSumUniq(arr, target);
+            // CountBinarySubstrings("001101");
+            string s = "Let's take LeetCode contest";
+            List<List<int>> l = new List<List<int>>();
+            List<int> x = new List<int> { 1, 1, 1 };
+            l.Add(x);
+             x = new List<int> { 0, 1, 0 };
+            l.Add(x);
+            x = new List<int> { 0, 0, 0 };
+            l.Add(x);
+            x = new List<int> { 1, 1, 0 };
+            l.Add(x);
+            //  gridProblem(l);
+            //  simpleCipher("vtaog",2);
+            // simpleCipher1("vtaog", 2);
+            //  ReverseWords(s);
+            string mono = "THTHTH";
+            // MinFlipsMonoIncr(mono);
+            // int[] team = { 3, 4, 3, 1, 6, 5 };int teamSize = 3;
+            // countTeams(team,teamSize);
+            string test = "abcabc";
+            // findAllKDisctintChars(test, 3);
+            //   var result= print_all_sum();
+            test obj = new test();
+            // obj.printSubArrays();
+            int[] nums = { 1, 2, 3, 4, 5, 4, 1 };
+            int maxSize = 4;
+            // obj.printLRUMisses(nums, maxSize);
+            int[] arr1= { 100, 100, 80, 70, 60 }; int cutOffrank = 4;
+          //  obj.CutOffRank(arr1, cutOffrank);
+            int[] abilites = {1,2,3,4 };
+            int noOfProccess = 15;
+            int numberOfProcessors = abilites.Length;
+
+            // Console.WriteLine(obj.MultiProcessors(numberOfProcessors, abilites, noOfProccess));
+            int[] coins = { 1, 2, 5 }; int amount = 11;
+            Console.WriteLine(obj.CoinChange(coins,amount));
+        }
+
+     
+        private static List<string> findAllKDisctintChars(string s,int k)
+        {
+            //lets say s is awaglknagawunagwkwagl and k=4;
+           
+            List<string> res = new List<string>();
+           
+                for (int i = 0; i <= s.Length - k; i++)
+                {
+                string sub = s.Substring(i, k);
+                    if (!checkIfContainRepeat(sub))
+                    res.Add(s.Substring(i, k));
+                }
+            
+            return res;
+
+        }
+
+      
+        private static bool checkIfContainRepeat(string s)
+        {
+            HashSet<char> set = new HashSet<char>();
+            //char[] ch = s.ToCharArray();
+            foreach(var ch in s)
+            {
+                if (!set.Contains(ch))
+                {
+                    set.Add(ch);
+                }
+                else
+                {
+                    //the given substring has a character which is already prsent in the hashSet,hence it doesnot have unique characters
+                    //so return false;
+                    return true;
+                }
+            }
+            return false;
+        }
+        private static int countTeams(int[] team,int teamSize)
+        {
+            int res = 0;
+            Array.Sort(team);
+            int i = 0, j = teamSize - 1;
+            while (j < team.Length)
+            {
+                if (team[j] - team[i] <= 2)
+                {
+                    //means we found a team
+                    res++;
+                    i = j + 1;
+                    j = j+ teamSize;
+                }
+                else
+                {
+                    i++;j++;
+                }
+            }
+
+            return res;
+        }
+
+        
+
+        public static int NumPairsDivisibleBy60(int[] time)
+        {
+
+            int res = 0;
+            Dictionary<int, int> dicReminder = new Dictionary<int, int>();
+            foreach (var t in time)
+            {
+                int remainder = t % 60;//
+                int target = 60 - remainder;//，
+                if (dicReminder.ContainsKey(target))//
+                {
+                    res += dicReminder[target];
+                }
+                if (remainder == 0)
+                {
+                    remainder = 60;
+                }
+
+                if (!dicReminder.ContainsKey(remainder))
+                {
+                    dicReminder.Add(remainder, 1);
+                }
+                else
+                {
+                    dicReminder[remainder] = dicReminder[remainder] + 1;
+                }
+
+            }
+            return res;
+        }
+
+        public static int twoSumUniq(int[] nums,int target)
+        {
+
+            Array.Sort(nums);
+            int i = 0;
+            int j = nums.Length - 1;
+            int ans = 0;
+            while (i < j)
+            {
+                int sum = nums[i] + nums[j];
+                if (sum < target)
+                {
+                    i++;
+                }
+                else if (sum > target)
+                {
+                    j--;
+                }
+                else
+                {
+                    ans++;
+                    i++;
+                    j--;
+                    while (i < j && nums[i] == nums[i - 1])
+                    {
+                        i++;
+                    }
+                    while (i < j && nums[j] == nums[j + 1])
+                    {
+                        j--;
+                    }
+                }
+            }
+            return ans;
+        }
+        public static int CountBinarySubstrings(string s)
+        {
+            int prevRunLength = 0, curRunLength = 1, res = 0;
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s.ElementAt(i) == s.ElementAt(i - 1)) curRunLength++;
+                else
+                {
+                    prevRunLength = curRunLength;
+                    curRunLength = 1;
+                }
+                if (prevRunLength >= curRunLength) res++;
+            }
+            return res;
+        }
+        public static string ReverseWords(string s)
+        {
+
+            var buff = new StringBuilder();
+            var stack = new Stack<char>();
+            foreach (char c in s)
+            {
+                if (c == ' ')
+                {
+                    while (stack.TryPop(out char v))
+                    {
+                        buff.Append(v);
+                    }
+                    buff.Append(' ');
+                }
+                else
+                {
+                    stack.Push(c);
+                }
+            }
+            while (stack.TryPop(out char v))
+            {
+                buff.Append(v);
+            }
+            return buff.ToString();
+        }
+        public static String simpleCipher(String encrypted, int k)
+        {
+            char[] _encrypted = encrypted.ToCharArray();
+            for (int i = 0; i < encrypted.Length; i++)
+            {
+                char x = _encrypted[i];
+                // if the previous kth element is greater than 'A'
+                if (x - k >= 65)
+                {
+                    _encrypted[i] = (char)(x - k);
+                }
+                //if ascii code of kth previous element if less than that of A add 26 to it
+                else
+                {
+                    _encrypted[i] = (char)(x - k + 26);
+                }
+            }
+            Console.Write(new String(_encrypted));
+            return new String(_encrypted);
+        }
+
+        public static int MinFlipsMonoIncr(string S)
+        {
+            int H = 0, increasing_seq = 0; //HHTHTT
+
+            for (int i = 0; i < S.Length; i++)
+            {
+                if ((S[i] - 'H') == 0)
+                    H++;
+                else
+                    increasing_seq++;
+
+                increasing_seq = Math.Max(increasing_seq, H);
+            }
+            Console.WriteLine(S.Length - increasing_seq);
+            return S.Length - increasing_seq;
+        }
+
+//        Ts = flips = 0
+//for c in s:
+//    if c == 'T':
+//        Ts += 1
+//    else:
+//        flips = min(flips+1, Ts)
+//return flips
+        public static int gridProblem(List<List<int>> grid)
+        {
+
+            int ans = 0;
+            int sum=0, prev = 0;
+          foreach(var ele in grid)
+            {
+                sum = 0;
+                var row = ele;
+                foreach(var number in row)
+                {
+                    sum = sum + number;
+                }
+                if (sum > 0)
+                {
+                    ans = ans + prev * sum;
+                    prev = sum;
+                }
+            }
+
+            return ans;
+        }
+
+
+        static void print_all_sum_rec( int target, int current_sum,   int start, List<List<int>> output,  List<int> result)
+        {
+
+            if (target == current_sum)
+            {
+               result.RemoveAt(0);
+                output.Add(result);
+            }
+
+            for (int i = start; i < target; ++i)
+            {
+                int temp_sum = current_sum + i;
+                if (temp_sum <= target)
+                {
+
+                    result.Add(i);
+                    print_all_sum_rec(target, temp_sum, i, output, result);
+                    result.Remove(result.Count() - 1);
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+        static List<List<int>>  print_all_sum()
+        {
+            int target = 4;
+            List<List<int>> output = new List<List<int>>();
+            List<int> result = new List<int>();
+            print_all_sum_rec(target, 0, 1, output, result);
+          
+            return output;
+        }
+    }
+}
---
+++ misc/test.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace misc
+{
+    public class test
+    {
+        //1/ Palindrome break/
+        public string breakPalidrone(string s)
+        {
+            if (s.Length == 0 || s.Length <= 1) return "";
+            char[] ch = s.ToCharArray();
+            int i = 0;
+            for (; i < ch.Length / 2; i++)
+            {
+                if (ch[i] != 'a')
+                {
+                    ch[i] = 'a';
+                    break; // break out as we have successfully replaced it with the smallest character.
+                }
+            }
+            if (i == s.Length / 2)
+            {
+                //means it could not replace chars with 'a'
+                //then make the last chacrater b.
+                ch[s.Length - 1] = 'b';
+            }
+            return new string(ch);
+        }
+        public int noOfSwapsToSort(int[] arr)
+        {
+            int count = 0;
+            int[] temp = new int[arr.Length];
+            Array.Copy(temp, arr, arr.Length);
+            Array.Sort(temp);
+            //now check if the elements are not in the correct position,if not just swap with the correct position.
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (temp[i] != arr[i])
+                {
+                    //means position is not right.
+                    count++;
+                    int correctIndex = getIndexOfElement(arr, temp[i]);
+                    swap(arr, i, correctIndex);
+                }
+            }
+
+
+            return count;
+        }
+        private int getIndexOfElement(int[] arr, int ele)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == ele)
+                {
+                    return i;
+                    break;
+                }
+            }
+            return -1;
+        }
+        private void swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        public void printSubArrays()
+        {
+            int[] arr = { 1, 2, 3 };
+            string res = "";
+            for (int i = 0; i < arr.Length; i++)
+            {
+                res = "";
+                for (int j = i; j < arr.Length; j++)
+                {
+                    res = res + "  " + arr[j];
+                    Console.WriteLine(res);
+                }
+            }
+        }
+        public void printLRUMisses(int[] arr, int maxSize)
+        {
+            if (arr == null) return;
+            int count = 0;
+            List<int> lst = new List<int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (lst.Contains(arr[i]))
+                {
+                    lst.Remove(arr[i]);
+                }
+                else
+                {
+                    //it is a miss.so increment the count
+                    count++;
+                    //if the size of the array is more than the cache size.,then remove the first element from the lst 
+                    if (lst.Count == maxSize)
+                    {
+                        lst.RemoveAt(0);
+                    }
+
+
+                }
+                lst.Add(arr[i]);
+            }
+            Console.WriteLine($"the no of misses is :{count}");
+        }
+        public class Debt
+        {
+            public String borrower = "";
+            public String lender = "";
+            public int amount = 0;
+
+            Debt(String b, String l, int a)
+            {
+                borrower = b;
+                lender = l;
+                amount = a;
+            }
+        }
+        //debt  -->
+        //Borrower    Lender   Amt  
+        //Alex        blake     2
+        //Blake       Alex      2
+
+        public List<string> negativeDebt(List<Debt> records)
+        {
+            List<string> res = new List<string>();
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (Debt record in records)
+            {
+
+                if (dict.ContainsKey(record.lender))
+                {
+                    dict[record.lender] = dict[record.lender] + record.amount;
+                }
+                else
+                {
+                    dict.Add(record.lender, 0);
+                }
+                if (dict.ContainsKey(record.borrower))
+                {
+                    dict[record.borrower] = dict[record.borrower] - record.amount;
+                }
+                else
+                {
+                    dict.Add(record.borrower, 0);
+                }
+            }
+
+            var list = dict.OrderBy(p => p.Value).ToList();
+            int min = list.FirstOrDefault().Value;
+            if (min >= 0)
+            {
+                //means there is no negative balace.
+                return res;
+            }
+
+            foreach (var ele in dict)
+            {
+                res.Add(ele.Key);
+            }
+            res.Sort();
+            return res;
+
+        }
+        public int CutOffRank(int[] arr, int cutOffrank)
+        {
+            int res = 0;
+            Array.Sort(arr);
+            List<Student> students = new List<Student>();
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            foreach (var ele in arr)
+            {
+                if (dict.ContainsKey(ele))
+                {
+                    dict[ele]++;
+                }
+                else
+                {
+                    dict.Add(ele, 1);
+                }
+            }
+            int index = 1;
+            var response = dict.OrderByDescending(p => p.Key).ToList();
+            foreach (var ele in response)
+            {
+                int noOfStudents = ele.Value;
+                Student objS = new Student();
+                for (int i = 0; i < noOfStudents; i++)
+                {
+                    objS.Id = ele.Key;
+                    objS.Rank = index;
+                    students.Add(objS);
+                }
+                index = index + noOfStudents; //next rank.
+
+            }
+
+
+            foreach (var ele in students)
+            {
+                Console.WriteLine("Student with ID " + ele.Id + " has a  rank of  = :" + ele.Rank);
+
+            }
+            var result = students.Where(p => p.Rank <= cutOffrank).ToList();
+
+            Console.WriteLine("======================");
+            foreach (var ele in result)
+            {
+                Console.WriteLine(ele.Id + "rank is :" + ele.Rank);
+            }
+
+            return result.Count;
+        }
+
+        public class Student
+        {
+            public int Id { get; set; }
+            public int Rank { get; set; }
+        }
+
+        /////////////////////////////////////////////////
+        ///MultiProcessor Problem-->Class Priority Queue.
+        ///
+        private static bool isElement(int i)
+        {
+            return true;
+        }
+        public int MultiProcessors(int num, int[] ability, int noOfProcesses)
+        {
+            List<int> pq = new List<int>();
+            ability = ability.OrderByDescending(p => p).ToArray();
+            int countPerSecond = 0;
+            foreach (var ele in ability)
+            {
+                pq.Add(ele);
+
+
+            }
+
+            while (noOfProcesses > 0)
+            {
+                int abi = pq.ElementAt(0);
+                pq.Remove(abi);
+                noOfProcesses = noOfProcesses - abi;
+                pq.Add(abi / 2);
+                countPerSecond++;
+                pq = pq.OrderByDescending(p => p).ToList();
+            }
+            return countPerSecond;
+        }
+        public class customComparer : IComparer<int>
+        {
+            public int Compare(int x, int y)
+            {
+                if (x == y)
+                {
+                    return 0;
+                }
+                if (y >= x)
+                {
+                    return 1;
+                }
+                return -1;
+            }
+        }
+
+        public int CoinChange(int[] coins, int amount)
+        {
+            if (amount < 1) return 0;
+            int[] visit = new int[amount];
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(amount);
+             int idx = 1;
+            int count = 0;
+            while (queue.Count!=0)
+            {
+                count++;
+                int size = idx; idx = 0;
+                for (int i = 0; i < size; i++)
+                {
+                    int cur = queue.Dequeue();
+                    foreach(var item in coins)
+                    {
+                        int rem = cur - item;
+                        if (rem == 0) return count;
+                        if (rem > 0 && visit[rem] == 0)
+                        {
+                            queue.Enqueue(rem);
+                            visit[rem] = 1;
+                            idx++;
+                        }
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
